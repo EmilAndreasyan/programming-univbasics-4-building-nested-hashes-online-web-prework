f4606bd8d24dc53fb6f55197566ec1b468ae5dd2
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-03 00:39:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash[:railroads] = [:pieces]
+  base_hash[:railroads] = :pieces
 end
 
 def monopoly_with_third_tier
-  base_hash[:railroads => {:pieces}]
+monopoly_with_second_tier[:railroads]  = :rent_in_dollars
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
